2a462855ff06c478a9493d0216d29b3f1c6aabb9
Cha14ka <taitor@yandex.ru>
2019-12-14 18:57:24 +0300

    Рабочий лонгполл и запуск плагинов(без потоков)

--- main.py
+++ main.py
@@ -13,6 +13,28 @@ log('{0} версии {1} от Augmeneco'.format(config['name'],config['version'
 plugins = []
 for file in os.listdir('plugins'):
     if os.path.isfile('plugins/'+file) and '.py' in file and file not in ['utils.py']:
-        plugin = importlib.import_module('plugins.'+file.replace('.py','')).main()
+        plugin = importlib.import_module('plugins.'+file.replace('.py',''))
         plugins.append(plugin)
-        log('Загружен плагин {0}, \n\tПрава: {1}\n\tКоманды: {2}'.format(file.replace('.py',''),plugin.level,plugin.keywords),0)
+        log('Загружен плагин {0}, \n\tПрава: {1}\n\tКоманды: {2}'.format(file.replace('.py',''),plugin.main.level,plugin.main.keywords),0)
+
+params = {'access_token':config['group_token'],'v':'5.103','group_id':config['id']}
+lpserver = requests.post('https://api.vk.com/method/groups.getLongPollServer',data=params,timeout=100).json()['response']
+ts = lpserver['ts']
+
+while(True):
+    try:
+        response = requests.post('{0}?act=a_check&key={1}&ts={2}&wait=25'.format(lpserver['server'],lpserver['key'],ts)).json()
+        ts = response['ts']
+    except Exception as error: print(error)
+    for updates in response['updates']:
+        if updates['type'] == 'message_new':
+            updates = updates['object']['message']
+            msg = updates
+            msg['toho'] = updates['peer_id']
+            msg['userid'] = updates['from_id']
+            
+            cmdinfo = iscommand(msg['text'],plugins)
+            if cmdinfo != False:
+                log('Вызвана команда '+cmdinfo['cmd']+' с текстом: '+msg['text'],msg['toho'])
+                plugin = cmdinfo['plugin'].main()
+                plugin.execute(msg)
--- plugins/utils.py
+++ plugins/utils.py
@@ -3,8 +3,8 @@ import requests, json, os, sys, subprocess, time, datetime, threading
 config = json.loads(open('data/config.json','r').read())
 
 def log(text,num=0):
-
     print(datetime.datetime.today().strftime("%H:%M:%S")+' | ['+str(num)+'] '+text)
+
 def chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]
@@ -23,3 +23,29 @@ def apisay(text,toho,attachment=None,keyboard={"buttons":[],"one_time":True},pho
     for chunk in chunks(text,4096):
         requests.post('https://api.vk.com/method/messages.send',data={'access_token':token,'v':'5.80','peer_id':toho,'message':chunk,'attachment':attachment,'keyboard':json.dumps(keyboard,ensure_ascii=False)}).json()    
     return True
+
+def iscommand(text,plugins):
+    text_split = text.split(' ')
+    result = {}
+    cmd = False
+
+    if text_split[0][0] == '/' and text_split[0][1:] in config['names']:
+        cmd = text_split[1]
+    if text_split[0][0] == '/':
+        if text_split[0][1:] not in config['names']:
+            cmd = text_split[0][1:]
+    if text_split[0] in config['names']:
+        cmd = text_split[1]
+    if text_split[0] not in config['names'] and text_split[0][0] != '/':
+        cmd = text_split[0]
+
+    if cmd == False: return cmd
+    for plugin in plugins:
+        if cmd in plugin.main.keywords:
+            result['plugin'] = plugin
+            result['cmd'] = cmd
+            return result
+    
+    return False
+
+
